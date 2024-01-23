d279dab1272468341049c9f5335b4e076175392e
jen <jboucher154@gmail.com>
2024-01-23 17:52:54 +0200

    add delivery distance fee calculations and related tests, add MACROS for all fee and distance parameters

--- app/main.py
+++ app/main.py
@@ -4,17 +4,21 @@ from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field
 import datetime
 import iso8601
+import delivery_fee_macros
 
 # macro to attach debugger debugpy 1.8.0
 DEBUG = True
 
 app = FastAPI(title="Delivery Fee API")
 
+'''
+OrderDetails class maps to fields in POST request body for delivery fee calculation
+'''
 class OrderDetails(BaseModel):
 	cart_value: int = Field(description="The total cart value in cents", ge=0)
 	delivery_distance: int = Field(descirption="The distance between the store and customer's location in meters", ge=0)
 	number_of_items: int = Field(description="The number of items in the customer's shopping cart", ge=0)
-	time_str: str = Field(description="Order time in UTC in ISO format")
+	time: str = Field(description="Order time in UTC in ISO format")
 
 	model_config = {
 		"json_schema_extra": {
@@ -23,32 +27,56 @@ class OrderDetails(BaseModel):
 					"cart_value":790,
 					"delivery_distance":2235,
 					"number_of_items":4,
-					"time_str":"2024-01-15T13:00:00Z",
+					"time":"2024-01-15T13:00:00Z",
 				}
 			]
 		}
 	}
 
-# inclusing of 1900 or just till 1859?
-# is beyond today?
-def is_friday_rush_hours(order_time: str) -> bool:
-	rush_hour_range = range(15, 19)
+'''
+# is_rush_hours
+input: string of ISO formated date, e.g. "2024-01-15T13:00:00Z"
+output: bool indicating if time of order is during rush hours
+'''
+def is_rush_hours(order_time: str) -> bool:
+	rush_hour_range = range(delivery_fee_macros.RUSH_HOUR_BEGIN, delivery_fee_macros.RUSH_HOUR_END)
 	order_time_converted = iso8601.parse_date(order_time)
-	if order_time_converted.weekday() == 4 and order_time_converted.hour in rush_hour_range:
+	if order_time_converted.weekday() == delivery_fee_macros.RUSH_DAY and order_time_converted.hour in rush_hour_range:
 		print(order_time_converted.hour, ":", order_time_converted.min)
 		return True
 	return False
 
+'''
+# calculate_distance_fees
+input: int of total delivery distance in meters
+output: int of total delivery fees for distance in cents
+- uses BASE_DISTANCE_FEE, BASE_DISTANCE, ADDITIONAL_DISTANCE_UNIT, ADDITIONAL_DISTANCE_FEE
+to calculate all fees for distance
+'''
+def calculate_distance_fees(delivery_distance: int) -> int:
+	fees = delivery_fee_macros.BASE_DISTANCE_FEE
+	extra_fees = 0
+
+	if (delivery_distance > delivery_fee_macros.BASE_DISTANCE):
+		distance = delivery_distance - delivery_fee_macros.BASE_DISTANCE
+		num_extra_fees = distance // delivery_fee_macros.ADDITIONAL_DISTANCE_UNIT
+		num_extra_fees += 0 if distance % delivery_fee_macros.ADDITIONAL_DISTANCE_UNIT == 0 else 1
+		extra_fees = delivery_fee_macros.ADDITIONAL_DISTANCE_FEE * num_extra_fees
+	return fees + extra_fees
+
 @app.post("/delivery-fee") #what uri? snake case?
 def calculate_delivery_fee(order_details: OrderDetails) -> Response:
+	fee = 0
 	try:
-		is_rush_hour_order = is_friday_rush_hours(order_details.time_str)
-		print("Order day is friday: ", is_rush_hour_order)
+		if (order_details.cart_value < delivery_fee_macros.FREE_THRESHOLD):
+			fee = 2000
+		#modifiers after total is known
+		is_rush_hour_order = is_rush_hours(order_details.time)
 	except iso8601.iso8601.ParseError as e:
-		return JSONResponse(status_code=422, content={"error": "invalid date formating"}) #how to correctly return error case?
-	
-	return JSONResponse(status_code=201, content={"delivery_fee": "Hello!"})
-
+		return JSONResponse(status_code=422, content={"msg": "invalid date formating. provide iso8601 formatting in UTC"}) #how to correctly return error case?
+	if fee > delivery_fee_macros.MAXIMUM_DELIVERY_FEE:
+		fee = delivery_fee_macros.MAXIMUM_DELIVERY_FEE
+	return JSONResponse(status_code=201, content={"delivery_fee": fee})
 
 
 
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,35 +1,86 @@
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
-from app.main import is_friday_rush_hours, app
+from main import is_rush_hours, app, calculate_distance_fees
+import pytest
+import json
+import delivery_fee_macros
 
 def get_client():
 	return TestClient(app)
 
 '''
-#  Tests for `is_friday_rush_hours' function
+#  Tests for `is_rush_hours' function
+- tests written for Rush hours friday 15:00 - 19:00
 '''
-def test_is_friday_rush_hours_not_friday():
-	test_date = "2024-01-15T13:00:00Z"
-	result = is_friday_rush_hours(test_date)
-	assert(result == False)
+@pytest.mark.parametrize("not_rush_hours", [
+	"2024-01-15T13:00:00Z",
+	"2024-01-21T15:00:00Z",
+	"2024-01-19T13:00:00Z",
+	"2024-01-19T19:00:00Z"
+])
 
-def test_is_friday_rush_hours_is_friday():
-	test_date = "2024-01-19T13:00:00Z"
-	result = is_friday_rush_hours(test_date)
-	assert(result == False)
+def test_is_rush_hours_not_rush_hours(not_rush_hours):
+	assert is_rush_hours(not_rush_hours) ==  False
 
-def test_is_friday_rush_hours_is_friday_rush_hours():
-	test_date = "2024-01-19T15:00:00Z"
-	result = is_friday_rush_hours(test_date)
-	assert(result == True)
+@pytest.mark.parametrize("rush_hours", [
+	"2024-01-19T15:00:00Z",
+	"2024-01-12T16:00:00Z",
+	"2024-01-5T18:59:00Z",
+	"2024-01-5T15:01:00Z"
+])
+def test_is_friday_rush_hours_rush_hours(rush_hours):
+	assert is_rush_hours(rush_hours) == True
 
-def test_is_friday_rush_hours_not_friday_rush_hours():
-	test_date = "2024-01-21T15:00:00Z"
-	result = is_friday_rush_hours(test_date)
-	assert(result == False)
 
-# tests for bad dates (old, future, not formatted corrrectly) , on edge of range times
+# NOTE: tests for bad dates (old, future, not formatted corrrectly) , on edge of range times
+
+'''
+#  Tests for `calculate_distance_fees' function
+- test writtin with BASE_DISTANCE of 1000 meters before addtional fees
+'''
+
+@pytest.mark.parametrize("min_fees", [
+	0, 50, 500, 900, 999, 1000
+])
+def test_calculate_distance_fees_minimum_fees(min_fees):
+	assert calculate_distance_fees(min_fees) == delivery_fee_macros.BASE_DISTANCE_FEE
+
+@pytest.mark.parametrize("larger_fees", [
+	1001, 1300, 1500, 1501, 2000, 2499
+])
+def test_calculate_distance_fees_not_minimum_fees(larger_fees):
+	assert calculate_distance_fees(larger_fees) != delivery_fee_macros.BASE_DISTANCE_FEE
+
+def test_calculate_distance_fees_one_extra_fee():
+	distance = 1001
+	assert calculate_distance_fees(distance) == delivery_fee_macros.BASE_DISTANCE_FEE + delivery_fee_macros.ADDITIONAL_DISTANCE_FEE
+
+def test_calculate_distance_fees_two_extra_fees():
+	distance = 1501
+	assert calculate_distance_fees(distance) == delivery_fee_macros.BASE_DISTANCE_FEE + (delivery_fee_macros.ADDITIONAL_DISTANCE_FEE * 2)
+
 
 '''
 #	Tests for POST method
 '''
+
+def test_free_delivery_yes():
+	client = get_client()
+	testPost = {"cart_value": 20000, "delivery_distance": 2235, "number_of_items": 4, "time": "2024-01-15T13:00:00Z"}
+	response = client.post("/delivery-fee", json = testPost)
+	assert response.status_code == 201
+	assert response.json() == {"delivery_fee": 0}
+
+def test_free_delivery_no():
+	client = get_client()
+	testPost = {"cart_value": 2000, "delivery_distance": 2235, "number_of_items": 4, "time": "2024-01-15T13:00:00Z"}
+	response = client.post("/delivery-fee", json = testPost)
+	assert response.status_code == 201
+	assert response.json()["delivery_fee"] != 0
+
+def test_max_delivery_price():
+	client = get_client()
+	testPost = {"cart_value": 2000, "delivery_distance": 9235, "number_of_items": 13, "time": "2024-01-15T13:00:00Z"}
+	response = client.post("/delivery-fee", json = testPost)
+	assert response.status_code == 201
+	assert response.json()["delivery_fee"] == delivery_fee_macros.MAXIMUM_DELIVERY_FEE
